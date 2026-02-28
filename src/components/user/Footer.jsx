e40abeab4812696492f568226e356ae76ffcfdf8
stephenbech <stephenbech19@gmail.com>
2026-02-28 22:44:51 +0100

    create terms &policy page

--- src/components/user/Footer.jsx
+++ src/components/user/Footer.jsx
@@ -1,12 +1,11 @@
-import { FiPhone, FiMail, FiMapPin } from "react-icons/fi";
+import { FiMail, FiMapPin, FiPhone } from "react-icons/fi";
 
 const Footer = () => {
   return (
-        <footer className="bg-slate-200 py-12">
-          <div className="max-w-7xl mx-auto px-4">
+    <footer className="bg-slate-200 py-12">
+      <div className="max-w-7xl mx-auto px-4">
         {/* Top grid */}
         <div className="grid grid-cols-1 md:grid-cols-4 gap-8">
-          
           {/* Brand & tagline */}
           <div>
             <a href="/" className="flex items-center space-x-3 mb-4">
@@ -20,18 +19,24 @@ const Footer = () => {
               Making restaurant reservations simple and enjoyable
             </p>
           </div>
-          
+
           {/* Explore */}
           <div>
             <h3 className="text-lg font-semibold leading-snug mb-4">Explore</h3>
             <ul className="space-y-2 text-gray-600">
               <li>
-                <a href="/restaurants" className="hover:text-gray-900 text-gray-900 text-base font-normal leading-normal">
+                <a
+                  href="/restaurants"
+                  className="hover:text-gray-900 text-gray-900 text-base font-normal leading-normal"
+                >
                   Restaurants
                 </a>
               </li>
               <li>
-                <a href="/hotels" className="hover:text-gray-900  text-gray-900 text-base font-normal leading-normal">
+                <a
+                  href="/hotels"
+                  className="hover:text-gray-900  text-gray-900 text-base font-normal leading-normal"
+                >
                   Hotels
                 </a>
               </li>
@@ -45,7 +50,7 @@ const Footer = () => {
               </li>
             </ul>
           </div>
-          
+
           {/* Quick Links */}
           <div>
             <h3 className="text-lg font-semibold leading-snug mb-4">
@@ -53,28 +58,40 @@ const Footer = () => {
             </h3>
             <ul className="space-y-2 text-gray-600">
               <li>
-                <a href="/about" className="hover:text-gray-900  text-gray-900 text-base font-normal leading-normal">
+                <a
+                  href="/about"
+                  className="hover:text-gray-900  text-gray-900 text-base font-normal leading-normal"
+                >
                   About us
                 </a>
               </li>
               <li>
-                <a href="/contact" className="hover:text-gray-900  text-gray-900 text-base font-normal leading-normal">
+                <a
+                  href="/contact"
+                  className="hover:text-gray-900  text-gray-900 text-base font-normal leading-normal"
+                >
                   Contact
                 </a>
               </li>
               <li>
-                <a href="/faqs" className="hover:text-gray-900  text-gray-900 text-base font-normal leading-normal">
+                <a
+                  href="/faqs"
+                  className="hover:text-gray-900  text-gray-900 text-base font-normal leading-normal"
+                >
                   FAQs
                 </a>
               </li>
               <li>
-                <a href="/help" className="hover:text-gray-900  text-gray-900 text-base font-normal leading-normal">
+                <a
+                  href="/help"
+                  className="hover:text-gray-900  text-gray-900 text-base font-normal leading-normal"
+                >
                   Help center
                 </a>
               </li>
             </ul>
           </div>
-          
+
           {/* Contact */}
           <div>
             <h3 className="text-lg font-semibold leading-snug mb-4">Contact</h3>
@@ -95,17 +112,25 @@ const Footer = () => {
 
         {/* Bottom bar */}
         <div className="mt-12 border-t border-slate-300 pt-8 flex flex-col md:flex-row justify-between items-center ">
-          <p className="text-gray-900 text-base font-normal leading-normal">© 2025 Bookies. All Rights Reserved</p>
+          <p className="text-gray-900 text-base font-normal leading-normal">
+            © 2025 Rhace. All Rights Reserved
+          </p>
           <div className="flex space-x-6 mt-4 md:mt-0">
-            <a href="/privacy" className="hover:text-gray-900 text-gray-900 text-base font-normal leading-normal">
+            <a
+              href="/privacy-policy"
+              className="hover:text-gray-900 text-gray-900 text-base font-normal leading-normal"
+            >
               Privacy Policy
             </a>
-            <a href="/terms" className="hover:text-gray-900 text-gray-900 text-base font-normal leading-normal">
+            <a
+              href="/terms"
+              className="hover:text-gray-900 text-gray-900 text-base font-normal leading-normal"
+            >
               Terms of Service
             </a>
           </div>
+        </div>
       </div>
-          </div>
     </footer>
   );
 };
